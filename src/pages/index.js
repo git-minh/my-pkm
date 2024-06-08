def71d38ea26058960fb322c37061aa03cfdf619
git-minh <71268298+git-minh@users.noreply.github.com>
2024-06-08 21:17:04 +0700

    update new Folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/Foundations of Agile & Agile Frameworks/Agile">
+            to="/docs/Project Management/Foundations of Agile & Agile Frameworks/Agile">
             Docusaurus Tutorial - 5min ⏱️
           </Link>
         </div>
